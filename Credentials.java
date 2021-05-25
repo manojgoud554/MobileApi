18bf78240c2b01ca7008876ef863dedd511fe430
Anurag1132 <64855654+Anurag1132@users.noreply.github.com>
2021-05-24 22:29:18 -0400

    Add files via upload

---
+++ Credentials.java
@@ -0,0 +1,8 @@
+package com.apiprojects.musicapp;
+
+public class Credentials {
+
+    public static final  String Base_Url="https://genius.p.rapidapi.com/";
+    public static final String Api_Key="0b5014bca2d395d4d7fda9f951987cae";
+
+}
